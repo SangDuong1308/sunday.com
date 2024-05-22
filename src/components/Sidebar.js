64dad864c7cbe7477537703962cb40f4ca526311
Duong Phuoc Sang <duongphuocsang1308@gmail.com>
2024-05-22 20:09:03 +0700

    Side bar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,3 +1,6 @@
+import { NavLink } from "react-router-dom";
+import DashBoardIcon from "../assets/dashboard_icon.svg";
+import AddIcon from "../assets/add_icon.svg";
 import "./Sidebar.css";
 
 import React from "react";
@@ -6,7 +9,30 @@ export default function Sidebar() {
   return (
     <div className="sidebar">
       <div className="sidebar-content">
-        <div className="user">{/* avatar and username */}</div>
+        <div className="user">
+          {/* avatar and username */}
+          <p>Hey user</p>
+        </div>
+        <nav className="links">
+          <ul>
+            <li>
+              <NavLink exact to="/" className="side-links">
+                <img
+                  className="side-icon"
+                  src={DashBoardIcon}
+                  alt="Dashboard Icon"
+                />
+                <span>Dashboard</span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/create" className="side-links">
+                <img className="side-icon" src={AddIcon} alt="Add icon" />
+                <span>New Project</span>
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   );
