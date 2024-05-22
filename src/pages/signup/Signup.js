6d44b368cb4a2311a57b0c3e6714876933d3c9fa
Duong Phuoc Sang <duongphuocsang1308@gmail.com>
2024-05-22 20:40:19 +0700

    sign up form

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,7 +1,48 @@
 import "./Signup.css";
 
-import React from "react";
+import React, { useState } from "react";
 
 export default function Signup() {
-  return <div>Signup</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [thumbnail, setThumbnail] = useState(null);
+
+  return (
+    <form className="auth-form">
+      <h2>Sign up</h2>
+      <label>
+        <span>Email:</span>
+        <input
+          required
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        ></input>
+      </label>
+      <label>
+        <span>Password:</span>
+        <input
+          required
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        ></input>
+      </label>
+      <label>
+        <span>Name:</span>
+        <input
+          required
+          type="text"
+          value={displayName}
+          onChange={(e) => setDisplayName(e.target.value)}
+        ></input>
+      </label>
+      <label>
+        <span>Profile Thumbnail:</span>
+        <input require type="file"></input>
+      </label>
+      <button className="btn">Sign Up</button>
+    </form>
+  );
 }
