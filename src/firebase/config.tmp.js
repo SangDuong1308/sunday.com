70ae29b41dd28d5a8a96f89c66a69813fea1e74e
Duong Phuoc Sang <duongphuocsang1308@gmail.com>
2024-05-23 23:15:24 +0700

    firebase config fix

--- src/firebase/config.tmp.js
+++ src/firebase/config.tmp.js
@@ -3,14 +3,7 @@ import { getFirestore, Timestamp } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDpEoT_2cNXWbZ0CSj3iuYzFn0VI-5DYcI",
-  authDomain: "garlicthecheem.firebaseapp.com",
-  projectId: "garlicthecheem",
-  storageBucket: "garlicthecheem.appspot.com",
-  messagingSenderId: "872743507856",
-  appId: "1:872743507856:web:6e63e9345ada18a68fc515",
-};
+const firebaseConfig = {};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
