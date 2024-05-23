626b87aa24fa6f85edc2c9ba009c72c1cf4c3f42
Duong Phuoc Sang <duongphuocsang1308@gmail.com>
2024-05-23 10:30:38 +0700

    Redirects, Route Guards

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./pages/dashboard/Dashboard";
 import Create from "./pages/create/Create";
 import Login from "./pages/login/Login";
@@ -8,31 +8,52 @@ import Signup from "./pages/signup/Signup";
 import Project from "./pages/project/Project";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 function App() {
+  const { authIsReady, user } = useAuthContext();
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Sidebar />
-        <div className="container">
-          <Navbar />
-          <Routes>
-            <Route exact path="/" element={<Dashboard />}></Route>
-          </Routes>
-          <Routes>
-            <Route path="/create" element={<Create />}></Route>
-          </Routes>
-          <Routes>
-            <Route path="/projects/:id" element={<Project />}></Route>
-          </Routes>
-          <Routes>
-            <Route path="/login" element={<Login />}></Route>
-          </Routes>
-          <Routes>
-            <Route path="/signup" element={<Signup />}></Route>
-          </Routes>
-        </div>
-      </BrowserRouter>
+      {authIsReady && (
+        <BrowserRouter>
+          <Sidebar />
+          <div className="container">
+            <Navbar />
+            <Routes>
+              <Route
+                exact
+                path="/"
+                element={user ? <Dashboard /> : <Navigate to="/login" />}
+              ></Route>
+            </Routes>
+            <Routes>
+              <Route
+                path="/create"
+                element={user ? <Create /> : <Navigate to="/login" />}
+              ></Route>
+            </Routes>
+            <Routes>
+              <Route
+                path="/projects/:id"
+                element={user ? <Project /> : <Navigate to="/" />}
+              ></Route>
+            </Routes>
+            <Routes>
+              <Route
+                path="/login"
+                element={user ? <Navigate to="/" /> : <Login />}
+              ></Route>
+            </Routes>
+            <Routes>
+              <Route
+                path="/signup"
+                element={user ? <Navigate to="/" /> : <Signup />}
+              ></Route>
+            </Routes>
+          </div>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,40 +4,56 @@ import Temple from "../assets/temple.svg";
 import React from "react";
 import { Link } from "react-router-dom";
 import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 export default function Navbar() {
-  const { logout, error, isPending } = useLogout();
+  const { logout, isPending } = useLogout();
+  const { user } = useAuthContext();
 
   return (
     <div className="navbar">
       <ul>
-        <li className="logo">
-          <img
-            width="36"
-            height="36"
-            src="https://img.icons8.com/badges/36/adventures.png"
-            alt="adventures"
-          />
-          <span>sunday.com</span>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/signup">Signup</Link>
-        </li>
-        <li>
-          {!isPending && (
-            <button className="btn" onClick={logout}>
-              Logout
-            </button>
-          )}
-          {isPending && (
-            <button className="btn" disabled>
-              Loging out...
-            </button>
-          )}
-        </li>
+        {!user && (
+          <>
+            {" "}
+            <li className="logo">
+              <Link to="/" className="nav-link">
+                <img
+                  className="nav-icon"
+                  width="36"
+                  height="36"
+                  src="https://img.icons8.com/badges/36/adventures.png"
+                  alt="adventures"
+                />
+                <span>sunday.com</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+          </>
+        )}
+
+        {user && (
+          <>
+            <li style={{ marginRight: "20px" }}>Hello {user.displayName}</li>
+            <li>
+              {!isPending && (
+                <button className="btn" onClick={logout}>
+                  Logout
+                </button>
+              )}
+              {isPending && (
+                <button className="btn" disabled>
+                  Loging out...
+                </button>
+              )}
+            </li>
+          </>
+        )}
       </ul>
     </div>
   );
